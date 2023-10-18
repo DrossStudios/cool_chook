0a450a8a1c627ec1ad950674191932c11a69cfd7
DrossStudios <dross_studios@proton.me>
2023-10-18 01:02:43 -0600

    Made a few tweaks so code matches hardware

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from machine import ADC, Pin, PWM
 from time import time
 from ds1302 import DS1302
 
-###### Borrowed Classes ######
-
 ###### Original Classes ######
 class Temps:
 	'''This class manages all temprature-themed variables, functions, formatting, etc.'''
@@ -44,8 +42,8 @@ class PWM_Dev():
 			-tuple, containing either a pair of:
 				integers (min and max temp readings)
 				-or-
-				a string ("day", "night", or "24") and an integer between 0 and 3600
-				NOTE: in a string/integrr pair, negative (-) numbers will be treated as 0, and numbers >3600 will be treated as 3600
+				a string ("day", "night", or "24") and an integer between 0 and 3599
+				NOTE: in a string/integrr pair, negative (-) numbers will be treated as 0, and numbers >3599 will be treated as 3599
 		seas_on,
 			-tuple, containing 2 strings from the following 5: "spring", "summer", "autumn", "winter", "all"
 				NOTE: during __init__, if either value is "all", the 1st will be rewritten as "all" and the 2nd will be null-string("")
@@ -53,13 +51,14 @@ class PWM_Dev():
 	def __init__(self,
 			pin, 			# GPIO pin number
 			temp_or_time,	# String: "heat", "cool", or "time"
-			_on_off, 		# Tuple: 2 integers reresenting min/max temps; or a string ("day","night","24") and an integer (0-3600)
+			_on_off, 		# Tuple: 2 integers reresenting min/max temps; or a string ("day","night","24") and an integer (0-3599)
 			seas_on,		# Tuple: any 2 ("spring", "summer", "autumn", "winter", "all")
 			):
 		self.pin_id = PWM(Pin(pin))
 		self.pin_id.freq(300) # fan-specific; may need adjustment for other devices.
 		self.pin_id.duty_u16(65535) # this number is the max value that can be used
 		self.trigger_mode = temp_or_time.lower()
+		self.fade_end = "0:0"
 		if type(_on_off[0]) == int and _on_off[0] > _on_off[1]: _on_off = (_on_off[1],_on_off[0])
 		if self.trigger_mode == "cool":
 			self.dev_on = _on_off[1]
@@ -68,6 +67,7 @@ class PWM_Dev():
 			self.dev_on = _on_off[0]
 			# calculate the fade-finish times as 0600hrs or 1800hrs + _on_off[1] -- hour % 12 == 6 then min >= (_on_off[1] // 60) then second >= _on_off[1] % 60
 			self.dev_off = _on_off[1]
+			self.fade_end = f"{_on_off[1]//60}:{_on_off[1]%60}"
 		if seas_on[0].lower() == "all" or seas_on[1].lower() == "all":
 			seas_on = ("all","")
 		self.season_on = seas_on[0]
@@ -142,8 +142,8 @@ class Device_Info:
 				-tuple, containing either a pair of:
 					integers (min and max temp readings)
 					-or-
-					a string ("day", "night", or "24") and an integer between 0 and 3600
-					NOTE: in a string/integrr pair, negative (-) numbers will be treated as 0, and numbers >3600 will be treated as 3600
+					a string ("day", "night", or "24") and an integer between 0 and 3599
+					NOTE: in a string/integrr pair, negative (-) numbers will be treated as 0, and numbers >3599 will be treated as 3599
 			seas_on,
 				-tuple, containing 2 strings from the following 5: "spring", "summer", "autumn", "winter", "all"
 					NOTE: during __init__, if either value is "all", the 1st will be rewritten as "all" and the 2nd will be null-string("")
@@ -152,9 +152,9 @@ class Device_Info:
 		self.Pull 		= PWM_Dev(2, "cool", (70, 90), ("all", ""))
 		self.Circulation= PWM_Dev(4, "heat", (50, 80), ("all", ""))
 		self.Heat 		= PWM_Dev(6, "heat", (35, 35), ("all", ""))
-		self.Tan		= PWM_Dev(8, "time", ("day", 1800), ("summer", "autumn"))
-		self.Light		= PWM_Dev(25, "time", ("day", 1800), ("all", ""))
-		self.RTC 		= DS1302(Pin(11),Pin(12),Pin(13))
+		self.Light		= PWM_Dev(8, "time", ("day", 1800), ("all", ""))
+		self.Tan		= PWM_Dev(10, "time", ("day", 1800), ("summer", "autumn"))
+		self.RTC 		= DS1302(Pin(13),Pin(14),Pin(15))
 		self.RTC.start()
 	# end of method
 
@@ -178,7 +178,7 @@ Test_cool = PWM_Dev(0,"cool", (66,75), ("Summer","Autumn"))
 Test_time = PWM_Dev(0,"time", ("day",30), ("Summer","Autumn"))
 Coop = Device_Info()
 # Comment-out these commands as-needed for proper configuration
-#Coop.RTC.date_time([2023, 9, 20, 3, 9, 0, 0]) # set datetime for 20 Sep 2023, 09:00:00. (A Wednesday, the 4th day of the week >> value 3)
+Coop.RTC.date_time([2023, 10, 15, 1, 11, 15, 0]) # set datetime for 15 Oct 2023, 11:15:00. (A Sunday, the 1st day of the week >> value 3)
 
 ### Test-Code lines
 print(Coop.RTC.date_time())
