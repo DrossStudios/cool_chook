88d084eadd59b9d373363e156ce848c968aadc96
DrossStudios <dross_studios@proton.me>
2023-10-23 09:41:30 -0600

    finalized the pulse-demo code and template

--- infini_pulse_demo.py
+++ infini_pulse_demo.py
@@ -1,6 +1,14 @@
-import time
+from time import *
 from machine import ADC, Pin, PWM
 
-led = Pin(25, Pin.OUT)
-led.value(1)
+led = PWM(Pin(25)) # use the on-board LED to demonstrate the PWM pulse
+led.freq(1000)
+led.duty_u16(1)
 
+while True:
+    for i in range(65536):
+        led.duty_u16(i)
+        sleep(0.1)
+    for i in range(65535, -1,-1):
+        led.duty_u16(i)
+        sleep(0.1)
