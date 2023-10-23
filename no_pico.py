4af53871322fd3369cd58f6f62ce8551d06899b8
DrossStudios <dross_studios@proton.me>
2023-10-23 09:31:22 -0600

    created a trouble-shooting sandbox file

---
+++ no_pico.py
@@ -0,0 +1,8 @@
+### This file is for figuring out my errors in python (sans the "machine" module testing)
+
+print("forward counting 'for' loop")
+for i in range(30):
+    print(i)
+print("backward counting 'for' loop")
+for i in range(29, -1, -1):
+    print(i)
