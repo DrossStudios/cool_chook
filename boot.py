6867466528db7c8f456697455e455573fd9f6eca
DrossStudios <124641443+DrossStudios@users.noreply.github.com>
2023-10-25 11:14:02 -0600

    draft progresson boot.py

--- boot.py
+++ boot.py
@@ -1,9 +1,23 @@
 '''This file contains tests, imports, and logs required to ensure a smooth run every time'''
 
+bootmsg = ""
+errmsg = ""
+
 try:
-    with open("log_file.txt", "a") as log_file:
-        # append date/time for boot, and any errors above
-        pass
+    import ds1302
+except ImportError:
+    pass
+    # write a py file that uses onboard RTC to mimic board
+    # log this error
+#except: other errors
+#    log and (if able) correct
+#else:
+#    whatever's needed
+
+try:
+    bootlog = open("boot_log.txt", "a")
+    # append date/time for boot, and any errors above
+    pass
 except FileNotFoundError: 
     # create the log file and provide it's initial data
     pass
