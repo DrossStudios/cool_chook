422a29b11a4131bbb600a91c7b923be7e1b02768
DrossStudios <dross_studios@proton.me>
2023-10-27 16:50:26 -0600

    Updated boot.py with RTC in place of datetime

--- boot.py
+++ boot.py
@@ -1,10 +1,11 @@
 '''This file contains tests, imports, and logs required to ensure a smooth run every time'''
-from machine import Pin, ADC, PWM
-from time import sleep, sleep_us
-from datetime import datetime, date, time,
+from machine import Pin, ADC, PWM, RTC
+from utime import sleep, sleep_us
+#from datetime import datetime, date, time # can't actually use datetime in uPy without manually loading the lib
 
 bootmsg = ""
 errmsg = ""
+rtc = RTC()
 
 try:
     from ds1302 import DS1302
@@ -18,8 +19,8 @@ except Exception as add_error:
 else:
     try:
         ds = DS1302(Pin(12),Pin(13),Pin(14))
-        # tuple passed to date_time is: (year, month, date, day of week, hour, minute, second)
-        # system default renders Monday as the 1st day of the week (0), not Sunday.
+        # tuple passed to ds1302.date_time is: (year, month, date, day of week, hour, minute, second)
+        # NOTE: system default renders Monday as the 1st day of the week (0), not Sunday; changing this is a future QOL goal
         if ds.year() == 2165:
             ds.date_time((2023, 10, 26, 3, 13, 0, 0))
         if ds.year() == 2165:
@@ -27,22 +28,24 @@ else:
     except ValueError as add_error:
         errmsg += f"Hardware Error:\n{add_error}\n\n"
     try:
-        ds.year() == datetime.now().strftime("%Y")
+        ds.year() == rtc.datetime()[0] # type: ignore
     except NameError as add_error:
         try:
             open("last_time.txt", "r")
         except FileNotFoundError as add_error:
             errmsg += f"FileNotFoundError: last_time.txt\n{add_error}\n\n"
-            datetime(2023, 10, 26, 14, 0, 0)
+            rtc.datetime((2023, 10, 26, 0, 14, 0, 0, 0))
             with open("last_time.txt", "w") as time_backup:
-                print(f"{date.year} {date.month} {date.day} {datetime.hour} {datetime.minute} {datetime.second}", file=time_backup)
+                date_now = rtc.datetime()
+                # date/time tuple sequence is as follows: (year, month, day, weekday, hours, minutes, seconds, subseconds)
+                print(f"{date_now[0]} {date_now[1]} {date_now[2]} {date_now[3]} {date_now[4]} {date_now[5]} {date_now[6]} {date_now[7]}", file=time_backup)
         else:
             with open("last_time.txt", "w") as time_backup:
                 time_in = time_backup.read().split()
-                datetime(time_in[0], time_in[1], time_in[2], time_in[3], time_in[4], time_in[5], time_in[6])
+                rtc.datetime(time_in[0], time_in[1], time_in[2], time_in[3], time_in[4], time_in[5], time_in[6], time_in[7])
     else:
-        if ds.year() != datetime.now().strftime("%Y"):
-            datetime(ds.year(), ds.month(), ds.day(), ds.hour(), ds.minute(), ds.second())
+        if ds.year() != rtc.datetime()[1]: # type: ignore
+            rtc.datetime((ds.year(), ds.month(), ds.day(), ds.weekday, ds.hour(), ds.minute(), ds.second(), 0)) # type: ignore
 # done trying to get the time clock to a reasonably accurate time
 
 try:
@@ -50,14 +53,14 @@ try:
 except FileNotFoundError as add_error: 
     # create the log file and provide it's initial data
     errmsg += f"FileNotFoundError: boot_log.txt missing; file created\n{add_error}\n\n"
-    with open("boot)log.txt", "w") as bootlog:
-        print(f"Cool Chooks Climate Controller\nBoot Log\n")
+    with open("boot_log.txt", "w") as bootlog:
+        print(f"Cool Chooks Climate Controller\nBoot Log\n", file=bootlog)
 except Exception as add_error:
     # unknown error reporting
     errmsg += f"Unexpected Error:\n{add_error}\n\n"
 finally:
     with open("boot_log.txt", "a") as bootlog:
-        boot_out = f"{datetime.now()} - Boot cycle complete\n"
+        boot_out = f"{rtc.datetime()} - Boot cycle complete\n" # type: ignore
         if errmsg == "": boot_out += f"Errors:\nNone"
         else: boot_out += f"Errors:\n{errmsg}"
         print(boot_out, file = bootlog)
