b67d06b6650ac9336ff09fc171f409e8393d82eb
DrossStudios <dross_studios@proton.me>
2023-10-30 14:32:50 -0600

    infinipulse final version completed

--- infini_pulse_demo.py
+++ infini_pulse_demo.py
@@ -1,6 +1,15 @@
-import time
-from machine import ADC, Pin, PWM
+from machine import Pin, PWM
+from ds1302 import *
+from utime import *
 
-led = Pin(25, Pin.OUT)
-led.value(1)
+led = PWM(Pin(0))
+led.freq(1000)
+led.duty_u16(1)
 
+while True:
+    for i in range(65536):
+        led.duty_u16(i)
+        sleep(0.0001)
+    for i in range(65535, -1,-1):
+        led.duty_u16(i)
+        sleep(0.0001)
